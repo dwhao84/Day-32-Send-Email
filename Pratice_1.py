5cb887a8fd6e4c05c229320b9d2a070837eb9578
Dawei Hao <dwsamurai84@gmail.com>
2024-10-23 08:08:49 +0800

    using linecache to read the txt file.

--- Pratice_1.py
+++ Pratice_1.py
@@ -1,6 +1,6 @@
-import smtplib
 from _datetime import datetime as dt
-from mail import yahoo_address
+from random import randint
+import linecache
 
 """
 Objective: Send a motivational quote via email on the current weekday.
@@ -9,11 +9,20 @@ Hints:
 1. Use the datetime module to obtain the current day of the week.
 2. Open the quotes.txt file and obtain a list of quotes.
 3. Use the random module to pick a random quote from your list of quotes.
-4. Use the smtplib to send the email to yourself.
+4. Use the matplotlib to send the email to yourself.
 """
 
+current_time = dt.now() # 2024/10/23
+
+random_num = randint(1, 102)
+
+QUOTES = "quotes.txt"
+random_quotes = linecache.getline(QUOTES, random_num)
+print(random_quotes)
+
 # Create SMTP(Simple Mail Transfer Protocol)
-s = smtplib.SMTP(yahoo_address, 587)
-# Start TLS for security
-s.starttls()
+# https://www.geeksforgeeks.org/send-mail-attachment-gmail-account-using-python/
+
+
+
 
